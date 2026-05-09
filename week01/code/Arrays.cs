c51bfb514f9b3a478fa91521e97c818b0d043929
Your Name <talionidas@hotmail.com>
2026-05-09 14:53:17 +0200

    Arrays Part 1+2

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,19 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //We beed a loop to iterate through the given lenght of the number in question, in this case i amount
+        //Within this loop we need to calculate the multiple of the selected number for each slot
+        //Each of these newly calculated multiples need to be stored in the array
+        //upon completion of the loop the finished array should be showed.
+
+        double[] result = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -29,5 +41,24 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //We need to figure out where the list needs to be split - that point will be data.Count - amount
+        //Create a list with the values from the split pointto the end of the original list which will then move to the front after the rotation.
+        //Create a second list with all values from the start of the original list up to the split point.
+        //Empty the original list so we can repopulate it
+        //Add the first new list and then afterwards the second new list
+
+        int splitPoint = data.Count - amount;
+
+        List<int> endPortion = data.GetRange(splitPoint, amount);
+
+        List<int> beginningPortion = data.GetRange(0, splitPoint);
+
+        data.Clear();
+
+        data.AddRange(endPortion);
+
+        data.AddRange(beginningPortion);
+
     }
 }
