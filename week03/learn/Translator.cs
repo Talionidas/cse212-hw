a79d6d0491e323c00c8a0c86123e512c777145fd
Your Name <talionidas@hotmail.com>
2026-05-21 20:18:26 +0200

    Learning W03

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -1,8 +1,8 @@
-public class Translator
+public class TranslatorSolution
 {
     public static void Run()
     {
-        var englishToGerman = new Translator();
+        var englishToGerman = new TranslatorSolution();
         englishToGerman.AddWord("House", "Haus");
         englishToGerman.AddWord("Car", "Auto");
         englishToGerman.AddWord("Plane", "Flugzeug");
@@ -13,28 +13,20 @@ public class Translator
 
     private Dictionary<string, string> _words = new();
 
-    /// <summary>
-    /// Add the translation from 'from_word' to 'to_word'
-    /// For example, in a english to german dictionary:
-    /// 
-    /// my_translator.AddWord("book","buch")
-    /// </summary>
-    /// <param name="fromWord">The word to translate from</param>
-    /// <param name="toWord">The word to translate to</param>
-    /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
-    /// <summary>
-    /// Translates the from word into the word that this stores as the translation
-    /// </summary>
-    /// <param name="fromWord">The word to translate</param>
-    /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        string newWord = "???";
+
+        if (_words.ContainsKey(fromWord))
+        {
+            newWord = _words[fromWord];
+        }
+
+        return newWord;
     }
 }
